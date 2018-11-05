fd4bd73263d1a301d50fadef02526105dbacc50b
sharonandisi <sharonandisi.28@gmail.com>
2018-11-05 05:56:59 +0300

    create user class

---
+++ python/password_locker/user.py
@@ -0,0 +1,18 @@
+class user:
+    """
+    this class holds user details
+    """
+    def __init__(self,login,password):
+        """
+        this creates credentials for the user
+        """
+        self.signin = signin
+        self.password = password
+
+    def user_existant(self,password):
+        """
+        this is for user authentification
+        """
+        if self.password == password:
+            return True
+        return False
